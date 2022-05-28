dc129969b85c2c809007d9c4a379b41e197c7ffa
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-05-27 23:35:11 -0500

    commit tree 9b65756512e362e70914b5cab0237a74c727ed8f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+/*function superbowlWin(record) {
+    if(record.result === "W") {
+        return record.year
+    } //else {
+        //return undefined;
+    }
+//}
+
+record.find(superbowlWin);*/
+
+function superbowlWin(record){
+
+    const superbowlWin = record.find(record => record.result === "W");
+    if (superbowlWin) {
+        return superbowlWin.year
+    } else {
+        return undefined;
+    }
+}    
